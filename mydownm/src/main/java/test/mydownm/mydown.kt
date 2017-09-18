2befd853e03134d231096815c173278fa945ba75
呵呵 <wangbiyu@easyretailpro.com>
2017-09-18 17:24:21 +0800

    列表底部半透明情况，点击半透明部分列表弹窗消失

--- mydownm/src/main/java/test/mydownm/mydown.kt
+++ mydownm/src/main/java/test/mydownm/mydown.kt
@@ -12,10 +12,7 @@ import android.widget.PopupWindow
 import android.widget.RelativeLayout
 import com.jude.easyrecyclerview.EasyRecyclerView
 import kotlinx.android.synthetic.main.main.view.*
-import org.jetbrains.anko.find
-import org.jetbrains.anko.matchParent
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.wrapContent
+import org.jetbrains.anko.*
 import java.util.*
 
 open class mydown : RelativeLayout {
@@ -29,9 +26,10 @@ open class mydown : RelativeLayout {
     var styy: Int? = 0//多种上部布局样式。0：为默认，文字居中，箭头在最右|1：文字和箭头一起居中
     var styybottom: Int? = 0//多种列表下部样式。0：为默认，列表宽度与上部相，列表到屏幕底部的部分全透|1：列表宽度为屏幕宽度，列表下部到屏幕下部半透明
 
-    constructor(context: Context, i: Int?) : super(context) {//代码创建控件
+    constructor(context: Context, i: Int?=0, j: Int?=0) : super(context) {//代码创建控件
         mContext = context
         styy = i
+        styybottom = j
         init()
     }
 
@@ -44,7 +42,7 @@ open class mydown : RelativeLayout {
     }
 
     // 动画
-    private fun startPropertyAnim(a: Float, b: Float) {
+    private fun startPropertyAnim(a: Float, b: Float) {//箭头的动画
         val anim = ObjectAnimator.ofFloat(rightimg, "rotation", a, b)
         anim.duration = 200
         anim.addUpdateListener { animation -> val value = animation.animatedValue as Float }
@@ -53,37 +51,53 @@ open class mydown : RelativeLayout {
 
     fun init() {
         // 准备弹出视图
-        if (styy == 0) {
+        if (styy == 0) {//不同的顶部视图样式
             viewmain = View.inflate(mContext, R.layout.main, null)
         } else if (styy == 1) {
             viewmain = View.inflate(mContext, R.layout.main2, null)
         }
-        viewPop = View.inflate(mContext, R.layout.easyrecyclerview_normal, null)
+
+        viewPop = View.inflate(mContext, R.layout.easyrecyclerview_halftrans, null)
         viewPop!!.find<EasyRecyclerView>(R.id.mydownrec).apply {
             //            addItemDecoration(itemDecoration)
             setLayoutManager(LinearLayoutManager(mContext))
         }
+        viewPop!!.find<RelativeLayout>(R.id.halfback).apply {
+            onClick {
+                pw?.dismiss()
+            }
+        }
         this@mydown.post {
-            test.mydownm.heightt=this@mydown.height
+            test.mydownm.heightt = this@mydown.height
             w = this@mydown.width
             h = getScreenHeight(context) - this@mydown.height - this@mydown.y.toInt() - getStatusHeight(context)
-            if (styybottom == 0) {
+            if (styybottom == 0) {//不同的列表高度，即显示不同的底部视图，透明和半透明
+                viewPop!!.find<RelativeLayout>(R.id.halfback).apply {
+                    background = ColorDrawable(0x00000000)
+                }
                 pw = PopupWindow(viewPop, w, wrapContent).apply {
                     //最正常没阴影的
+                    setBackgroundDrawable(ColorDrawable(0x00000000))
                     isFocusable = true//设置返回键关闭
                     isOutsideTouchable = true//设置菜单以外的点击生效 并闭菜单
                     setOnDismissListener { startPropertyAnim(180f, 0f) }
+                }
+                viewPop = View.inflate(mContext, R.layout.easyrecyclerview_normal, null)
+                viewPop!!.find<EasyRecyclerView>(R.id.mydownrec).apply {
+                    //            addItemDecoration(itemDecoration)
+                    setLayoutManager(LinearLayoutManager(mContext))
                 }
             } else {
                 pw = PopupWindow(viewPop, matchParent, h).apply {
-                    setBackgroundDrawable(ColorDrawable(0x55000000))
+                    setBackgroundDrawable(ColorDrawable(0x50000000))
                     isFocusable = true//设置返回键关闭
                     isOutsideTouchable = true//设置菜单以外的点击生效 并闭菜单
                     setOnDismissListener { startPropertyAnim(180f, 0f) }
-//                    onClick { dismiss() }
                 }
             }
         }
+
+        //列表适配器
         q = recycler_adapter(1, mContext)
 //        val itemDecoration = DividerDecoration(Color.parseColor("#ffe1e1e1"), 1, dip(18), dip(18))
 //        itemDecoration.setDrawLastItem(false)
@@ -96,21 +110,22 @@ open class mydown : RelativeLayout {
             pw?.showAsDropDown(this, 0, 0)
         }
         this@mydown.addView(viewmain)
-
     }
 
-    fun setmainback(back: GradientDrawable) {
+    fun setmainback(back: GradientDrawable) {//设置头部的背景
         this@mydown.background = back
         invalidate()
     }
 
-    fun setlistback(backk: GradientDrawable) {
+    fun setlistback(backk: GradientDrawable) {//设置列表的大背景
         viewPop!!.find<MyEasyRecyclerView>(R.id.mydownrec).apply {
             background = backk
         }
         invalidate()
     }
 
+
+    //列表点击的事件处理
     private fun <T> mydownclick(position: Int, bean: T?, callBack: Func1<T>?) {
         if (callBack != null) {
             try {
@@ -129,7 +144,7 @@ open class mydown : RelativeLayout {
         if (data != null && datamap == null) {//仅带字符串，前端写定的
             var w1 = ArrayList<selected_bean>()
             data.forEach { var temp = selected_bean();temp.mytext = it;w1.add(temp) }
-            w1[0].selected=true
+            w1[0].selected = true
             q?.addAll(w1)
             tmain.text = data[0]
             q?.setOnItemClickListener { position ->
@@ -145,7 +160,7 @@ open class mydown : RelativeLayout {
             datamap.forEach { var temp = selected_bean();temp.mytext = it.key;w0.add(temp) }
             q?.addAll(w0)
             var w = ArrayList<String>()
-            w0[0].selected=true
+            w0[0].selected = true
             w.addAll(datamap.keys)
             tmain.text = w[0]
             q?.setOnItemClickListener { position ->
